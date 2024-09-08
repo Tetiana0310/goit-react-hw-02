701992c276e3b70d8f71c979e675c9899176abf1
Tetiana Posmashna <tetiana.posmashna@gmail.com>
2024-09-08 17:40:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
 
 import Options from "./components/Options/Options";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    if (savedFeedback !== null) {
+      return JSON.parse(savedFeedback);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    }
+  })
+
+   useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+   }, [feedback]);
+  
   const updateFeedback = (feedbackType) => {
     setFeedback((prev) => ({
       ...prev,
